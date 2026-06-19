2033b616f6d8dced8738fe36bace602619ac9039
Jawad Sher <158135119+JawadSher@users.noreply.github.com>
2026-06-19 16:29:49 +0500

    Create solution.cpp

---
+++ 01 - Array & String/18 - Integer to Roman/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        string ROMAN = "";
+        vector<pair<int, string>> table = {
+            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+            {100, "C"},  {90, "XC"},  {50, "L"},  {40, "XL"},
+            {10, "X"},   {9, "IX"},   {5, "V"},   {4, "IV"},
+            {1, "I"}
+        };
+
+        for(auto& [value, symbol] : table){
+            while (num >= value){
+                ROMAN += symbol;
+                num -= value;
+            }
+        }
+
+        return ROMAN;
+    }
+};
