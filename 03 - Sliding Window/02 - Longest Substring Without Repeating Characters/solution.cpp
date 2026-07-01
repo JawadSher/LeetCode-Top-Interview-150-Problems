34e3a89056122f725520e1e4ab0fc4de472ca28a
Jawad Sher <158135119+JawadSher@users.noreply.github.com>
2026-07-01 20:40:39 +0500

    Implement optimized solution for longest substring problem
    Added an optimized O(N) solution for finding the length of the longest substring without repeating characters.

--- 03 - Sliding Window/02 - Longest Substring Without Repeating Characters/solution.cpp
+++ 03 - Sliding Window/02 - Longest Substring Without Repeating Characters/solution.cpp
@@ -28,3 +28,27 @@ public:
         return len;
     }
 };
+
+// O(N) Optimized Solution
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if(s.length() == 0) return 0;
+        if(s.length() == 1) return 1;
+
+        int j = 0;
+        unordered_map<char, int> last;
+        int len = 0;
+
+        for(int i = 0; i < s.length(); i++){
+            if(last.find(s[i]) != last.end()){
+                j = max(j, last[s[i]] + 1);
+            }
+
+            last[s[i]] = i;
+            len = max<int>(len, i - j + 1);
+        }
+
+        return len;
+    }
+};
